6ba09826c03368981651fdbcd62702aea832e4f6
fnajer <lexactr@gmail.com>
2019-07-30 18:13:03 +0300

    remove extend session upon initial loading

--- src/modules/App/epics/index.js
+++ src/modules/App/epics/index.js
@@ -3,12 +3,9 @@ import { of, forkJoin, concat } from 'rxjs'
 import { switchMap, catchError } from 'rxjs/operators'
 
 import { loadInitialInfo } from '../actions'
-import { updateAuthInfo } from 'modules/Auth/actions'
 import { updateLanguagesList, chooseLanguage } from 'modules/Header/actions'
 import { updateModelsList } from 'modules/LocalesManager/actions'
 
-import { extendSession } from 'modules/Auth/epics/extendSession'
-import { prepareSessionResponse } from 'modules/Auth/epics/loadAuth'
 import { prepareModelsResponse, fetchModels } from 'modules/LocalesManager/epics/models'
 import { fetchLanguages } from 'modules/Header/epics/languagesList'
 
@@ -19,8 +16,10 @@ const initialInfoEpic = action$ =>
     ofType(loadInitialInfo),
     switchMap(
       action => forkJoin(
-        extendSession(),
-        fetchLanguages()
+        fetchLanguages(),
+        fetchModels({
+          code: DEFAULT_LANGUAGE
+        })
       ),
       (action, responses) => {
         return [
@@ -28,25 +27,8 @@ const initialInfoEpic = action$ =>
         ]
       }
     ),
-    switchMap(
-      ([authResponse, languagesResponse]) => {
-        console.log(authResponse, languagesResponse)
-        if (authResponse)
-          return fetchModels({
-            code: DEFAULT_LANGUAGE
-          })
-        else 
-          return Promise.resolve(null)
-      },
-      ([authResponse, languagesResponse], modelsResponse) => [
-        authResponse,
-        languagesResponse,
-        modelsResponse
-      ]
-    ),
     switchMap(responses => {
       const [
-        authResponse,
         languagesResponse,
         modelsResponse,
       ] = responses
@@ -54,11 +36,9 @@ const initialInfoEpic = action$ =>
         lang.code === DEFAULT_LANGUAGE
       ))
 
-      const authData = prepareSessionResponse(authResponse)
       const modelsData = prepareModelsResponse(modelsResponse)
-
+  
       return concat(
-        of(updateAuthInfo(authData)),
         of(updateLanguagesList(languagesResponse.data)),
         of(chooseLanguage(defaultLanguage)),
         of(updateModelsList(modelsData))
